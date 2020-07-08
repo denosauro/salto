26bb77770e326b0c3ea3a8ebc131ffbcc72ecfdb
Tuneron <tunon2010@yandex.ua>
2020-07-08 19:10:09 +0300

    Refactor

--- src/main/java/com/denosauro/cardcontroller/service/salto/SaltoClient.java
+++ src/main/java/com/denosauro/cardcontroller/service/salto/SaltoClient.java
@@ -1,6 +1,7 @@
 package com.denosauro.cardcontroller.service.salto;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Arrays;
@@ -10,10 +11,9 @@ public class SaltoClient {
 
     public static void execute(String hostname, int port, SaltoCommand saltoCommand) throws IOException {
 
-        Scanner s = new Scanner(System.in);
-
         Socket socket = new Socket(hostname, port);
 
+        try {
             OutputStream output = socket.getOutputStream();
             DataOutputStream writer = new DataOutputStream(output);
 
@@ -22,9 +22,9 @@ public class SaltoClient {
 
             byte[] message = new byte[saltoCommand.serialize().length];
 
-            int j=0;
+            int j = 0;
 
-            for(Byte b: saltoCommand.serialize())
+            for (Byte b : saltoCommand.serialize())
                 message[j++] = b;
 
             System.out.println("Command : " + Arrays.toString(message));
@@ -35,7 +35,7 @@ public class SaltoClient {
 
             int answer = reader.read();
 
-            if(answer == SaltoConstants.ACK) {
+            if (answer == SaltoConstants.ACK) {
                 writer.writeInt(message.length);
                 if (message.length > 0) {
                     writer.write(message, 0, message.length);
@@ -43,6 +43,20 @@ public class SaltoClient {
                 System.out.println("Command send is done!");
             }
 
-            socket.close();
+            answer = reader.read();
+
+            if (answer == SaltoConstants.ACK) {
+                System.out.println("PMS request accepted and being processed");
+            }
+
+        } catch (IOException e) {
+            if (socket != null && !socket.isClosed()) {
+                try {
+                    socket.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace(System.err);
+                }
+            }
+        }
     }
 }
